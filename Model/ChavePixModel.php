661d017f86f4c227299b4a4b7254de01a92ac89e
Usuario <Usuario@CPD02-M04>
2023-04-27 14:47:22 -0300

    Arrumando DAO e Model de chave Pix

--- Model/ChavePixModel.php
+++ Model/ChavePixModel.php
@@ -3,3 +3,39 @@
 namespace ApiBanco_Digital\Model;
 
 use ApiBanco_Digital\DAO\ChavePixDAO;
+
+class ChavePixModel extends Model
+{
+    public $id, $chave, $tipo, $id_conta;
+
+    public function save()
+    {
+        $dao = new ChavePixDAO();
+
+        if($this->id == null)
+            $dao->insert($this);
+        else
+            $dao->update($this);
+    }
+
+    public function getAllRows()
+    {
+        $dao = new ChavePixDAO();
+
+        $this->rows = $dao->select();
+    }
+
+    public function getById(int $id)
+    {
+        $dao = new ChavePixDAO();
+
+        $this->rows = $dao->selectById($id);
+    }
+
+    public function delete(int $id)
+    {
+        $dao = new ChavePixDAO();
+
+        $dao->delete($id);
+    }
+}
