a56d4698a87b51ab254be0190fbccf006e562bc8
Adautoo10 <adauto.facin05@gmail.com>
2023-05-08 08:28:09 -0300

    Arrumando ROTAS

--- Rotas.php
+++ Rotas.php
@@ -1,29 +1,40 @@
 <?php
 
-use ApiBanco_Digital\Controller;
+use ApiBanco_Digital\Controller\
+{
+    ChavePixController,
+    ContaController,
+    CorrentistaController,
+    TransacaoController
+};
 
 
 $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
     switch($url)
     {
-        case '/correntista/save':        
+        case '/correntista/save': 
+            CorrentistaController::save();       
         break;
 
         case '/conta/extrato':
+            ContaController::Extrato();
         break;
 
         case '/conta/pix/enviar':
+            ContaController::Enviar();
         break;
 
         case 'conta/pix/receber':
+            ContaController::Receber();
         break;
 
         case '/correntista/entregar':
+            CorrentistaController::entrar();
         break;
 
         default:
         http_response_code(403);
-    break;
+        break;
 
     }
